869647f36399023216a87bdb82c7f5dbebf52774
shevchenkotaras <shevchenkotv91@gmail.com>
2017-01-08 21:38:43 +0200

    setting up

---
+++ Books/Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Entitites;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        public BooksDb(): base("DefaultConnecti")
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Books/Books.Web/DataContexts/IdnetityDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Books.Web.Models;
+
+namespace Books.Web.DataContexts
+{
+    public class IdntityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdntityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdntityDb Create()
+        {
+            return new IdntityDb();
+        }
+    }
+}
